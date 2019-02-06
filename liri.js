77342b9c025799c461fce2b04a959243f6a9835a
k8blanco <katiewhite303@gmail.com>
2019-02-06 16:51:06 -0700

    added colors, improved formatting

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 var axios = require("axios");
 var inquirer = require("inquirer");
 var Spotify = require("node-spotify-api");
+var colors = require("colors");
 var fs = require("fs");
 
 //spotify variables
@@ -40,7 +41,7 @@ inquirer.prompt([
                 let artistName = artist.artist; //this is the artist name
                 let artistFullName = artistName.split(" "); //this parses the artist name
                 let artistSearch = artistFullName.join("%20"); //this joins the artist name with %20 instead of spaces
-                console.log("\nSearching for " + artistName + " concerts");
+                console.log("\n...Searching for ".blue + artistName.blue + " concerts...".blue);
 
                 //fill bands in town query URL
                 let concertQuery = "https://rest.bandsintown.com/artists/" + artistSearch + "/events?app_id=codingbootcamp"
@@ -52,16 +53,16 @@ inquirer.prompt([
                     //format & display results 
                     if (response != 0) {
                         for (var i = 0; i < response.data.length; i++) {
-                            console.log("-----------------------------------------");
-                            console.log("\nVenue: " + response.data[i].venue.name);
+                            console.log("-----------------------------------------".blue);
+                            console.log("\nVenue: ".green + response.data[i].venue.name);
                             // console.log("\nDate: " + moment(response.data[i].datetime).format("MM/DD/YYY"));
                             //!!! need to add and format date of concerts here !!!
-                            console.log("\nCity: " + response.data[i].venue.city);
-                            console.log("\nState: " + response.data[i].venue.region + "\nCountry: " + response.data[i].venue.country);
+                            console.log("\nCity: ".green + response.data[i].venue.city);
+                            console.log("\nState: ".green + response.data[i].venue.region + "\nCountry: ".green + response.data[i].venue.country);
                              
                         }
                     } else {
-                        console.log("\nNo shows found for " + artistSearch);
+                        console.log("\nNo shows found for ".underline.red + artistSearch.underline.red);
                     };
                 }) 
                 //catch any errors
@@ -88,30 +89,26 @@ inquirer.prompt([
                 let songFullTitle = songTitle.split(" "); 
                 let songSearch = songFullTitle.join("%20");
                 
-                console.log("\nSearching for " + songTitle);
+                console.log("\n...Searching for ".blue + songTitle.blue + "...".blue);
 
-                //search spotify
+             
                 spotify.search({
                     type: "track",
                     query: songSearch,
                     limit: 1,
                 })
-                //format & display results
+              
                 .then(function(response) {
-                    // console.log(response.tracks);
-                    // var stringify = JSON.stringify(response);
-                    // console.log(stringify);
-                    
-                
+        
                     let song = response.tracks.items[0];
 
                     if (response.length != 0) {//this needs to change - response.length doesn't work here
-                        console.log("\n-----------------------------------------");
-                        console.log("\nSong: " + song.name);
-                        console.log("\nArtist: " + song.album.artists[0].name);
-                        console.log("\nAlbum: " + song.album.name);
-                        console.log("\nURL: " + song.preview_url);
-                        console.log("\n-----------------------------------------");
+                        console.log("\n-----------------------------------------".blue);
+                        console.log("\nSong: ".magenta + song.name);
+                        console.log("\nArtist: ".magenta + song.album.artists[0].name);
+                        console.log("\nAlbum: ".magenta + song.album.name);
+                        console.log("\nURL: ".magenta + song.preview_url);
+                        console.log("\n-----------------------------------------".blue);
                     } else {
                         //!! default to Ace of Base if song not found/no song entered !!
                         console.log("Need Ace of Base here");
@@ -142,7 +139,7 @@ inquirer.prompt([
                     let movieTitle = movie.movie;
                     let movieFullTitle = movieTitle.split(" ");
                     let movieSearch = movieFullTitle.join("%20");
-                    console.log("Searching for " + movieTitle);
+                    console.log("\n...Searching for ".blue + movieTitle.blue + "...".blue);
 
                     //fill query URL
                     let movieQuery = "http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=trilogy"
@@ -151,22 +148,22 @@ inquirer.prompt([
                     axios.get(movieQuery)
                     .then(function(response) {
 
-                        console.log("-----------------------------------------");
-                        console.log("\nMovie Title: " + response.data.Title);
-                        console.log("\nRelease Year: " + response.data.Year);
-                        console.log("\nimdb Rating: " + response.data.imdbRating);
-                        console.log("\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value); 
-                            console.log("** Guide to Rotten Tomatoes ratings: less than 60% is a 'splat,' or a bad review **");
-                        console.log("\nProduced in: " + response.data.Country);
-                        console.log("\nLanguage: " + response.data.Language);
-                        console.log("\nPlot Summary: " + response.data.Plot);
-                        console.log("\nActors: " + response.data.Actors);
-                        console.log("-----------------------------------------");
+                        console.log("\n-----------------------------------------".blue);
+                        console.log("\nMovie Title: ".green + response.data.Title);
+                        console.log("\nRelease Year: ".green + response.data.Year);
+                        console.log("\nimdb Rating: ".green + response.data.imdbRating);
+                        console.log("\nRotten Tomatoes Rating: ".green + response.data.Ratings[1].Value); 
+                            console.log("** Guide to Rotten Tomatoes ratings: less than 60% is a 'splat,' or a bad review **".red);
+                        console.log("\nProduced in: ".green + response.data.Country);
+                        console.log("\nLanguage: ".green + response.data.Language);
+                        console.log("\nPlot Summary: ".green + response.data.Plot);
+                        console.log("\nActors: ".green + response.data.Actors);
+                        console.log("\n-----------------------------------------".blue);
                         
                         //format and display results
                         if (response != 0) {//this needs to change, doesn't work
                             for (var i = 0; i < response.data.length; i++) {
-                                console.log("-----------------------------------------");
+                                console.log("\n-----------------------------------------");
                                 console.log(response)
                             }
                         } else {
@@ -182,14 +179,6 @@ inquirer.prompt([
                 break;
           
             
-                // * Title of the movie.  t
-                // * Year the movie came out. y
-                // * IMDB Rating of the movie.
-                // * Rotten Tomatoes Rating of the movie.
-                // * Country where the movie was produced.
-                // * Language of the movie.
-                // * Plot of the movie.
-                // * Actors in the movie.
 
         //call dowhatitsays code block
 
@@ -199,4 +188,14 @@ inquirer.prompt([
           
 
 
+
+// TO DO:
+    // add colors
+    // make recursive
+    // add moment/format concert date
+    // do whatitsays
+    // fix if statements
+    // default to Ace of Base - songs
+    // default to Mr Nobody - movies
+    // add in error catching for URL = null
 
